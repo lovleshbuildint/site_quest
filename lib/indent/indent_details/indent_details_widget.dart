6135452768ba38d1c271233b5f454f0e09d0ee54
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-18 11:38:52 +0000

    Updating to latest FlutterFlow output.

--- lib/indent/indent_details/indent_details_widget.dart
+++ lib/indent/indent_details/indent_details_widget.dart
@@ -257,7 +257,7 @@ class _IndentDetailsWidgetState extends State<IndentDetailsWidget> {
                                       Text(
                                         getJsonField(
                                           FFAppState().indentSelectedSite,
-                                          r'''$.BranchName''',
+                                          r'''$.CustomerBank''',
                                         ).toString(),
                                         style: FlutterFlowTheme.of(context)
                                             .bodyMedium

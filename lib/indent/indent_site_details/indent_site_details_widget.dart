a3d73d7420275e04d5066b94a30fb0b4f65ad897
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-19 08:52:27 +0000

    Updating to latest FlutterFlow output.

--- lib/indent/indent_site_details/indent_site_details_widget.dart
+++ lib/indent/indent_site_details/indent_site_details_widget.dart
@@ -699,16 +699,8 @@ class _IndentSiteDetailsWidgetState extends State<IndentSiteDetailsWidget> {
                             builder: (context, snapshot) {
                               // Customize what your widget looks like when it's loading.
                               if (!snapshot.hasData) {
-                                return Center(
-                                  child: SizedBox(
-                                    width: 50.0,
-                                    height: 50.0,
-                                    child: CircularProgressIndicator(
-                                      valueColor: AlwaysStoppedAnimation<Color>(
-                                        FlutterFlowTheme.of(context).primary,
-                                      ),
-                                    ),
-                                  ),
+                                return Image.asset(
+                                  '',
                                 );
                               }
                               final dropDownDashboardResponse = snapshot.data!;

cb02cfb1816b389e9c4aa9ca8a42f0c343beacdc
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-29 06:39:13 +0000

    Updating to latest FlutterFlow output.

--- lib/indent/indent_site_details/indent_site_details_widget.dart
+++ lib/indent/indent_site_details/indent_site_details_widget.dart
@@ -1478,6 +1478,10 @@ class _IndentSiteDetailsWidgetState extends State<IndentSiteDetailsWidget> {
                                       child: FutureBuilder<ApiCallResponse>(
                                         future: DistrictAPICall.call(
                                           token: FFAppState().Token,
+                                          istate: getJsonField(
+                                            FFAppState().indentSelectedSite,
+                                            r'''$.istate''',
+                                          ),
                                         ),
                                         builder: (context, snapshot) {
                                           // Customize what your widget looks like when it's loading.

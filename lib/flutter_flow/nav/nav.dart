9599b03c95004875e4797d7030b77d695460936a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-26 05:39:42 +0000

    Updating to latest FlutterFlow output.

--- lib/flutter_flow/nav/nav.dart
+++ lib/flutter_flow/nav/nav.dart
@@ -345,7 +345,7 @@ class FFParameters {
   // present is the special extra parameter reserved for the transition info.
   bool get isEmpty =>
       state.allParams.isEmpty ||
-      (state.extraMap.length == 1 &&
+      (state.allParams.length == 1 &&
           state.extraMap.containsKey(kTransitionInfoKey));
   bool isAsyncParam(MapEntry<String, dynamic> param) =>
       asyncParams.containsKey(param.key) && param.value is String;
@@ -366,9 +366,9 @@ class FFParameters {
 
   dynamic getParam<T>(
     String paramName,
-    ParamType type, [
+    ParamType type, {
     bool isList = false,
-  ]) {
+  }) {
     if (futureParamValues.containsKey(paramName)) {
       return futureParamValues[paramName];
     }

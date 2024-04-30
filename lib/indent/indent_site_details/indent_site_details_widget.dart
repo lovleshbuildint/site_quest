54d2101eae9f2e8ebd3c809b01280c074bcc9407
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-30 09:24:03 +0000

    Updating to latest FlutterFlow output.

--- lib/indent/indent_site_details/indent_site_details_widget.dart
+++ lib/indent/indent_site_details/indent_site_details_widget.dart
@@ -2458,7 +2458,10 @@ class _IndentSiteDetailsWidgetState extends State<IndentSiteDetailsWidget> {
                           padding: EdgeInsetsDirectional.fromSTEB(
                               0.0, 12.0, 0.0, 0.0),
                           child: Text(
-                            'Second Site Visited BY',
+                            valueOrDefault<String>(
+                              _model.iSecondSiteVisitedByName?.toString(),
+                              '1',
+                            ),
                             style: FlutterFlowTheme.of(context)
                                 .bodyMedium
                                 .override(

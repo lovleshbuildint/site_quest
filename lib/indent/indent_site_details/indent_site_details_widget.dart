88fa96643de75d264ea55a8f9d50596c3f5326a6
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-25 11:33:22 +0000

    Updating to latest FlutterFlow output.

--- lib/indent/indent_site_details/indent_site_details_widget.dart
+++ lib/indent/indent_site_details/indent_site_details_widget.dart
@@ -1653,10 +1653,7 @@ class _IndentSiteDetailsWidgetState extends State<IndentSiteDetailsWidget> {
                                         padding: EdgeInsetsDirectional.fromSTEB(
                                             0.0, 12.0, 0.0, 0.0),
                                         child: Text(
-                                          valueOrDefault<String>(
-                                            _model.stateValue,
-                                            'ssza',
-                                          ),
+                                          FFAppState().State,
                                           style: FlutterFlowTheme.of(context)
                                               .bodyMedium
                                               .override(

2d6c1b6aef6e9c6b5428315a45b341aa79cb3ac2
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-06 12:32:57 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/main_page/main_page_widget.dart
+++ lib/pages/main_page/main_page_widget.dart
@@ -1464,7 +1464,7 @@ class _MainPageWidgetState extends State<MainPageWidget> {
                                       highlightColor: Colors.transparent,
                                       onTap: () async {
                                         context.pushNamed(
-                                          'open_site_nearest_branch',
+                                          'indent_site_landlord_details',
                                           extra: <String, dynamic>{
                                             kTransitionInfoKey: TransitionInfo(
                                               hasTransition: true,

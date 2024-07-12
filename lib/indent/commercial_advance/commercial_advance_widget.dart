16076e1a35cbc87beaa7a224e6cca154814c49dd
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-12 09:39:49 +0000

    Updating to latest FlutterFlow output.

--- lib/indent/commercial_advance/commercial_advance_widget.dart
+++ lib/indent/commercial_advance/commercial_advance_widget.dart
@@ -1203,7 +1203,9 @@ class _CommercialAdvanceWidgetState extends State<CommercialAdvanceWidget> {
                     child: FlutterFlowDropDown<String>(
                       controller:
                           _model.securityDepositsdfghjValueController ??=
-                              FormFieldController<String>(null),
+                              FormFieldController<String>(
+                        _model.securityDepositsdfghjValue ??= '1',
+                      ),
                       options: ['1', '2', '3'],
                       onChanged: (val) async {
                         setState(() => _model.securityDepositsdfghjValue = val);
@@ -1249,6 +1251,12 @@ class _CommercialAdvanceWidgetState extends State<CommercialAdvanceWidget> {
                       isOverButton: true,
                       isSearchable: false,
                       isMultiSelect: false,
+                      labelText: '',
+                      labelTextStyle:
+                          FlutterFlowTheme.of(context).bodyMedium.override(
+                                fontFamily: 'Readex Pro',
+                                letterSpacing: 0.0,
+                              ),
                     ),
                   ),
                   Padding(

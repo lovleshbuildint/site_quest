329adabbfc9ad48af27dcddf4fda9db7e847bee0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-15 05:26:38 +0000

    Updating to latest FlutterFlow output.

--- lib/indent/commercial_advance/commercial_advance_widget.dart
+++ lib/indent/commercial_advance/commercial_advance_widget.dart
@@ -1186,7 +1186,10 @@ class _CommercialAdvanceWidgetState extends State<CommercialAdvanceWidget> {
                     padding:
                         EdgeInsetsDirectional.fromSTEB(0.0, 12.0, 0.0, 0.0),
                     child: Text(
-                      'Security Deposit (Months)',
+                      valueOrDefault<String>(
+                        _model.securityDepositsdfghjValue,
+                        'ttrd',
+                      ),
                       style: FlutterFlowTheme.of(context).bodyMedium.override(
                             fontFamily: 'Poppins',
                             color: Colors.black,

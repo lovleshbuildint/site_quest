07b566bc3eac2a74863f9f7ced89b0578cc8c82e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-06 06:44:12 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/main_page/main_page_widget.dart
+++ lib/pages/main_page/main_page_widget.dart
@@ -1466,7 +1466,7 @@ class _MainPageWidgetState extends State<MainPageWidget> {
                                       highlightColor: Colors.transparent,
                                       onTap: () async {
                                         context.pushNamed(
-                                          'indent_site_remark',
+                                          'open_site_commercial',
                                           queryParameters: {
                                             'updateData': serializeParam(
                                               false,
